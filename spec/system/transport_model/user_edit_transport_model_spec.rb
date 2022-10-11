b6457aed84964a2de5d4c91739257f0415d9890a
Thaís A <thais.antero@gmail.com>
2022-10-11 19:26:45 -0300

    Refatoração do arquivo de teste de edição dos modelos de transporte.

--- spec/system/transport_model/user_edit_transport_model_spec.rb
+++ spec/system/transport_model/user_edit_transport_model_spec.rb
@@ -15,7 +15,7 @@ describe 'Usuário edita um modelo de transporte' do
     expect(page).not_to have_link('Editar')
   end
 
-  it 'com sucesso' do
+  it 'visualizando dados cadastrados' do
     user = User.create!(name: 'Joao', email: 'joao@sistemadefrete.com.br', password: 'password', role: :admin)
     TransportModel.create(name: 'Express', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
                           maximum_weight: 10_000, fixed_rate: 10)
@@ -32,6 +32,27 @@ describe 'Usuário edita um modelo de transporte' do
     expect(page).to have_field('Peso Mínimo', with: 10)
     expect(page).to have_field('Peso Máximo', with: 10000)
     expect(page).to have_field('Taxa Fixa', with: 10)
+  end
+
+  it 'com sucesso' do
+    user = User.create!(name: 'Joao', email: 'joao@sistemadefrete.com.br', password: 'password', role: :admin)
+    TransportModel.create!(
+      name: 'Express', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+      maximum_weight: 10_000, fixed_rate: 10
+    )
+
+    login_as(user)
+    visit root_path
+    click_on 'Modelos de Transporte'
+    click_on 'Editar'
+    fill_in 'Nome', with: 'Sedex'
+    fill_in 'Distância Mínima', with: 100
+    fill_in 'Distância Máxima', with: 400
+    click_on 'Cadastrar'
+
+    expect(page).to have_content('Modelo de Transporte atualizado com sucesso.')
+    expect(page).to have_content('Sedex')
+    expect(page).to have_content('100 - 400km')
   end
 
   it 'e mantém os campos obrigatórios' do
