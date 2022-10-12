db25f535ff6b48aeff858d9551e005bbf31b18f0
Thaís A <thais.antero@gmail.com>
2022-10-12 17:17:45 -0300

    Implementa validações unitárias para que administrador preencha tabela de preço por distância da forma adequada. Para isso, foram incluídas validações padrões e customizadas no model de price_by_distance.

--- app/models/price_by_distance.rb
+++ app/models/price_by_distance.rb
@@ -1,3 +1,42 @@
 class PriceByDistance < ApplicationRecord
   belongs_to :transport_model
+
+  validates :start_range, :end_range, :distance_tax, presence: true
+  validates_uniqueness_of :start_range, :end_range
+  validates :end_range, numericality: { greater_than: :start_range }
+  validate :start_range_equal_past_end_range, :start_range_inside_range,
+           :end_range_inside_range, :end_range_greater_than_maximum_distance
+
+  def start_range_equal_past_end_range
+    past_end_range = PriceByDistance.where(transport_model_id: transport_model_id)
+                                    .where('end_range <= ?', start_range)
+                                    .maximum(:end_range) || transport_model.minimum_distance
+    return if past_end_range == start_range
+
+    errors.add(:start_range, "não pode ser diferente do valor da distância máxima do intervalo anterior #{past_end_range}")
+  end
+
+  def start_range_inside_range
+    prices_by_distances_of_transport_model = PriceByDistance.where(transport_model_id: transport_model_id)
+    ranges = prices_by_distances_of_transport_model.map { |i| (i.start_range + 1..i.end_range - 1) }
+
+    if ranges.any? { |i| i.include?(start_range) }
+      errors.add(:start_range, 'não pode estar incluso entre intervalos já cadastrados')
+    end
+  end
+
+  def end_range_inside_range
+    prices_by_distances_of_transport_model = PriceByDistance.where(transport_model_id: transport_model_id)
+    ranges = prices_by_distances_of_transport_model.map { |i| (i.start_range + 1..i.end_range - 1) }
+
+    if ranges.any? { |i| i.include?(end_range) }
+      errors.add(:end_range, 'não pode estar incluso entre intervalos já cadastrados')
+    end
+  end
+
+  def end_range_greater_than_maximum_distance
+    if end_range > transport_model.maximum_distance
+      errors.add(:end_range, 'não pode ser maior que máxima distância da modalidade de transporte')
+    end
+  end
 end
--- spec/models/price_by_distance_spec.rb
+++ spec/models/price_by_distance_spec.rb
@@ -1,4 +1,297 @@
 require 'rails_helper'
 
 RSpec.describe PriceByDistance, type: :model do
+  describe 'validações' do
+    context 'quando end_range menor ou igual a start_range' do
+      it 'é inválido' do
+        transport_model = TransportModel.create(
+          name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+          maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+        )
+        price_by_distance = PriceByDistance.new(
+          start_range: 60, end_range: 60,
+          distance_tax: 5, transport_model:
+        )
+
+        expect(price_by_distance).not_to be_valid
+      end
+
+      it 'adiciona erro' do
+        transport_model = TransportModel.create(
+          name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+          maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+        )
+        price_by_distance = PriceByDistance.new(
+          start_range: 60, end_range: 60,
+          distance_tax: 5, transport_model:
+        )
+
+        price_by_distance.validate
+
+        expect(price_by_distance.errors[:end_range]).to include "deve ser maior do que #{price_by_distance.start_range}"
+      end
+
+      context 'quando start_range é diferente do end_range do intervalo anterior' do
+        it 'é inválido' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          _price_by_distance_first = PriceByDistance.create(
+            start_range: 50, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+          price_by_distance_second = PriceByDistance.new(
+            start_range: 65, end_range: 70,
+            distance_tax: 5, transport_model:
+          )
+
+          expect(price_by_distance_second).not_to be_valid
+        end
+
+        it 'adiciona erro' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          price_by_distance_first = PriceByDistance.create(
+            start_range: 50, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+          price_by_distance_second = PriceByDistance.new(
+            start_range: 65, end_range: 70,
+            distance_tax: 5, transport_model:
+          )
+
+          price_by_distance_second.validate
+
+          expect(price_by_distance_second.errors[:start_range]).to include "não pode ser diferente do valor da distância máxima do intervalo anterior #{price_by_distance_first.end_range}"
+        end
+      end
+
+      context 'quando start_range é o primeiro a ser cadastrado e menor que a distância mínima' do
+        it 'é inválido' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          price_by_distance = PriceByDistance.new(
+            start_range: 40, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+
+          expect(price_by_distance).not_to be_valid
+        end
+
+        it 'adiciona erro' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          price_by_distance = PriceByDistance.new(
+            start_range: 40, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+
+          price_by_distance.validate
+
+          expect(price_by_distance.errors[:start_range]).to include "não pode ser diferente do valor da distância máxima do intervalo anterior #{transport_model.minimum_distance}"
+        end
+      end
+
+      context 'quando start_range está dentro de outro range cadatrado' do
+        it 'é inválido' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          _price_by_distance_first = PriceByDistance.create!(
+            start_range: 50, end_range: 70,
+            distance_tax: 5, transport_model:
+          )
+          price_by_distance_second = PriceByDistance.new(
+            start_range: 55, end_range: 65,
+            distance_tax: 5, transport_model:
+          )
+
+          expect(price_by_distance_second).not_to be_valid
+        end
+
+        it 'adiciona erro' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          price_by_distance_first = PriceByDistance.create(
+            start_range: 50, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+          price_by_distance_second = PriceByDistance.new(
+            start_range: 55, end_range: 70,
+            distance_tax: 5, transport_model:
+          )
+
+          price_by_distance_second.validate
+
+          expect(price_by_distance_second.errors[:start_range]).to include 'não pode estar incluso entre intervalos já cadastrados'
+        end
+      end
+
+      context 'quando end_range está dentro de outro range cadatrado' do
+        it 'é inválido' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          _price_by_distance_first = PriceByDistance.create!(
+            start_range: 50, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+          _price_by_distance_second = PriceByDistance.create!(
+            start_range: 60, end_range: 70,
+            distance_tax: 5, transport_model:
+          )
+          _price_by_distance_third = PriceByDistance.create!(
+            start_range: 70, end_range: 80,
+            distance_tax: 5, transport_model:
+          )
+          PriceByDistance.find(2).destroy
+          price_by_distance_forth = PriceByDistance.new(
+            start_range: 60, end_range: 75,
+            distance_tax: 5, transport_model:
+          )
+
+          expect(price_by_distance_forth).not_to be_valid
+        end
+
+        it 'adiciona erro' do
+          transport_model = TransportModel.create(
+            name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+            maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+          )
+          _price_by_distance_first = PriceByDistance.create!(
+            start_range: 50, end_range: 60,
+            distance_tax: 5, transport_model:
+          )
+          _price_by_distance_second = PriceByDistance.create!(
+            start_range: 60, end_range: 70,
+            distance_tax: 5, transport_model:
+          )
+          _price_by_distance_third = PriceByDistance.create!(
+            start_range: 70, end_range: 80,
+            distance_tax: 5, transport_model:
+          )
+          PriceByDistance.find(2).destroy
+          price_by_distance_forth = PriceByDistance.new(
+            start_range: 60, end_range: 75,
+            distance_tax: 5, transport_model:
+          )
+
+          price_by_distance_forth.validate
+
+          expect(price_by_distance_forth.errors[:end_range]).to include 'não pode estar incluso entre intervalos já cadastrados'
+        end
+
+        context 'quando end_range é maior do que máxima distância da modalidade de transporte' do
+          it 'é inválido' do
+            transport_model = TransportModel.create(
+              name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+              maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+            )
+            _price_by_distance_first = PriceByDistance.create!(
+              start_range: 50, end_range: 100,
+              distance_tax: 5, transport_model:
+            )
+            price_by_distance_second = PriceByDistance.new(
+              start_range: 100, end_range: 250,
+              distance_tax: 5, transport_model:
+            )
+
+            expect(price_by_distance_second).not_to be_valid
+          end
+
+          it 'adiciona erro' do
+            transport_model = TransportModel.create(
+              name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+              maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+            )
+            _price_by_distance_first = PriceByDistance.create!(
+              start_range: 50, end_range: 100,
+              distance_tax: 5, transport_model:
+            )
+            price_by_distance_second = PriceByDistance.new(
+              start_range: 100, end_range: 250,
+              distance_tax: 5, transport_model:
+            )
+
+            price_by_distance_second.validate
+
+            expect(price_by_distance_second.errors[:end_range]).to include 'não pode ser maior que máxima distância da modalidade de transporte'
+          end
+        end
+
+        context 'quando taxa não foi preenchida' do
+          it 'é inválido' do
+            transport_model = TransportModel.create(
+              name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+              maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+            )
+            price_by_distance_second = PriceByDistance.create(
+              start_range: 50, end_range: 150,
+              distance_tax: '', transport_model:
+            )
+
+            expect(price_by_distance_second).not_to be_valid
+          end
+        end
+
+        context 'quando start_range não for único' do
+          it 'é inválido' do
+            transport_model = TransportModel.create(
+              name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+              maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+            )
+            price_by_distance_second = PriceByDistance.create(
+              start_range: 50, end_range: 60,
+              distance_tax: 5, transport_model:
+            )
+            price_by_distance_second = PriceByDistance.create(
+              start_range: 50, end_range: 70,
+              distance_tax: 10, transport_model:
+            )
+
+            expect(price_by_distance_second).not_to be_valid
+          end
+        end
+        
+        context 'quando end_range não for único' do
+          it 'é inválido' do
+            transport_model = TransportModel.create(
+              name: 'Sedex', minimum_distance: 50, maximum_distance: 200, minimum_weight: 10,
+              maximum_weight: 10_000, fixed_rate: 10, status: 'active'
+            )
+            _price_by_distance_first = PriceByDistance.create!(
+              start_range: 50, end_range: 60,
+              distance_tax: 5, transport_model:
+            )
+            _price_by_distance_second = PriceByDistance.create!(
+              start_range: 60, end_range: 70,
+              distance_tax: 5, transport_model:
+            )
+            _price_by_distance_third = PriceByDistance.create!(
+              start_range: 70, end_range: 80,
+              distance_tax: 5, transport_model:
+            )
+            PriceByDistance.find(2).destroy
+            price_by_distance_forth = PriceByDistance.new(
+              start_range: 60, end_range: 80,
+              distance_tax: 5, transport_model:
+            )
+
+            expect(price_by_distance_forth).not_to be_valid
+          end
+        end
+      end
+    end
+  end
 end
