5ea12b9ed7ac1a01769b2924268273b4489f2ee1
Thaís A <thais.antero@gmail.com>
2022-10-14 16:25:49 -0300

    Implementa Refatorações de visualização de todas ordens de serviço

--- app/controllers/service_order_controller.rb
+++
@@ -1,7 +0,0 @@
-class ServiceOrderController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @service_orders = ServiceOrder.all.sort_by(&:service_order_status)
-  end
-end
--- spec/system/service_order/user_view_service_orders_spec.rb
+++ spec/system/service_order/user_view_service_orders_spec.rb
@@ -11,9 +11,10 @@ describe 'Usuário vê ordens de serviço cadastradas' do
   it 'sendo usuário regular' do
     user = User.create!(name: 'User', email: 'user@sistemadefrete.com.br', password: 'password', role: :regular_user)
     customer = Customer.create!(customer_address: 'Rua Padre Filó, 24', customer_cep: '60500344',
-                                customer_name: 'Ana Paula Silva Oliveira', customer_registration_number: '00355611224')
+      customer_name: 'Ana Paula Silva Oliveira', customer_registration_number: '00355611224')
+    allow(SecureRandom).to receive(:alphanumeric).and_return('ASDFGB456FGT7A7')
     service_order = ServiceOrder.create!(pickup_address: 'Rua Baronesa, 10', pickup_cep: '60400455', customer: customer,
-                                         delivery_distance: 70.5)
+                  delivery_distance: 70.5)
     Product.create!(length: 100, width: 70, height: 80, weight: 12.4, service_order: service_order)
 
     login_as(user)
@@ -21,13 +22,14 @@ describe 'Usuário vê ordens de serviço cadastradas' do
     click_on 'Ordens de Serviço'
 
     expect(page).not_to have_content('Nao existem ordens de serviço cadastradas')
-
+    expect(page).to have_content('Código')
     expect(page).to have_content('Endereço de Retirada')
     expect(page).to have_content('Dimensões do Produto(C x L x A)')
     expect(page).to have_content('Peso do Produto')
     expect(page).to have_content('Endereço de Entrega')
     expect(page).to have_content('Cliente (CPF)')
     expect(page).to have_content('Distância de Entrega')
+    expect(page).to have_content('ASDFGB456FGT7A7')
     expect(page).to have_content('Rua Baronesa, 10(60400-455)')
     expect(page).to have_content('100cm x 70cm x 80cm')
     expect(page).to have_content('12,40 kg')
@@ -40,6 +42,7 @@ describe 'Usuário vê ordens de serviço cadastradas' do
     user = User.create!(name: 'User', email: 'user@sistemadefrete.com.br', password: 'password', role: :admin)
     customer = Customer.create!(customer_address: 'Rua Padre Filó, 24', customer_cep: '60500344',
                                 customer_name: 'Ana Paula Silva Oliveira', customer_registration_number: '00355611224')
+    allow(SecureRandom).to receive(:alphanumeric).and_return('ASDFGB456FGT7A7')
     service_order = ServiceOrder.create!(pickup_address: 'Rua Baronesa, 10', pickup_cep: '60400455', customer: customer,
                                          delivery_distance: 70.5)
     Product.create!(length: 100, width: 70, height: 80, weight: 12.4, service_order: service_order)
@@ -49,13 +52,14 @@ describe 'Usuário vê ordens de serviço cadastradas' do
     click_on 'Ordens de Serviço'
 
     expect(page).not_to have_content('Nao existem ordens de serviço cadastradas')
-
+    expect(page).to have_content('Código')
     expect(page).to have_content('Endereço de Retirada')
     expect(page).to have_content('Dimensões do Produto(C x L x A)')
     expect(page).to have_content('Peso do Produto')
     expect(page).to have_content('Endereço de Entrega')
     expect(page).to have_content('Cliente (CPF)')
     expect(page).to have_content('Distância de Entrega')
+    expect(page).to have_content('ASDFGB456FGT7A7')
     expect(page).to have_content('Rua Baronesa, 10(60400-455)')
     expect(page).to have_content('100cm x 70cm x 80cm')
     expect(page).to have_content('12,40 kg')
