51d66a558d7b9b877610e190090f6d5d9e313307
Thaís A <thais.antero@gmail.com>
2022-10-16 07:49:08 -0300

    Adiciona campo de descrição da entrega na busca
    No caso da entrega for encerrada com atraso, a busca da ordem de pedido
    retorna a descrição da entrega com o motivo para o atraso.

--- spec/system/user_search_service_order_spec.rb
+++ spec/system/user_search_service_order_spec.rb
@@ -50,7 +50,8 @@ describe 'Usuário busca ordem de serviço' do
     estimated_delivery_date = Time.zone.now - 5.days
     service_order = ServiceOrder.create(pickup_address: 'Rua Baronesa, 10', pickup_cep: '60400455', customer:,
                                         delivery_distance: 55, transport_model:, vehicle:, estimated_delivery_date:,
-                                        service_order_status: :delivered, delivery_price: 40, delivery_date: Time.zone.now)
+                                        service_order_status: :delivered, delivery_price: 40, delivery_date: Time.zone.now,
+                                        delivery_status: :with_delay, delivery_description: 'Problema com veículo de entrega')
     Product.create(length: 100, width: 70, height: 80, weight: 12.4, service_order:)
 
     visit root_path
@@ -63,12 +64,14 @@ describe 'Usuário busca ordem de serviço' do
     expect(page).to have_content 'Placa de Identificação do Veículo'
     expect(page).to have_content 'Modelo do Veículo'
     expect(page).to have_content 'Data de Entrega'
+    expect(page).to have_content 'Descrição da Entrega'
     expect(page).to have_content 'Rua Baronesa, 10'
     expect(page).to have_content 'Rua Padre Filó, 24'
     expect(page).to have_content 'Encerrada'
     expect(page).to have_content 'PNG0000'
     expect(page).to have_content 'Van 1.6 Mi'
     expect(page).to have_content "#{Time.zone.now.strftime('%d/%m/%Y %H h')}"
+    expect(page).to have_content 'Problema com veículo de entrega'
   end
 
   it 'pendente' do
